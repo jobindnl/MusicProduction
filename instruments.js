66832464a8edd27258d2b0496a8309297ae70fe2
jobindnl <jobindnl@gmail.com>
2021-03-01 17:03:42 -0500

    added all instrument images

--- instruments.js
+++ instruments.js
@@ -1,11 +1,26 @@
 let photos = document.querySelectorAll("img")
-let prev = document.getElementById("prev")
-let next = document.getElementById("next")
-let slider = document.getElementById("slider")
+let prev = document.getElementsByClassName("prev")
+let next = document.getElementsByClassName("next")
+let slider = document.getElementsByClassName("slider")
+slider = slider[0]
 
-let counter = 1
+let counter = 0
 next.addEventListener("click", function (){
+    if(counter === 8 ){
+        return
+    }
+    prev.style.display = ""
     counter++
+    console.log(counter);
+    slider.style.transform = "translateX(" + -532 * counter + "px)"
+})
+
+prev.addEventListener("click", function (){
+    if(counter === 0){
+        return
+    }
+    counter--
+    console.log(counter);
     slider.style.transform = "translateX(" + -532 * counter + "px)"
 })
 
