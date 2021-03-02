8678f818249a8d5859c0fd22a7353e489eb983de
jobindnl <jobindnl@gmail.com>
2021-03-02 00:15:55 -0500

    minute changes

--- instruments.js
+++ instruments.js
@@ -3,24 +3,20 @@ let prev = document.getElementsByClassName("prev")
 let next = document.getElementsByClassName("next")
 let slider = document.getElementsByClassName("slider")
 slider = slider[0]
-
 let counter = 0
-next.addEventListener("click", function (){
+next[0].addEventListener("click", function (){
     if(counter === 8 ){
         return
     }
-    prev.style.display = ""
     counter++
-    console.log(counter);
     slider.style.transform = "translateX(" + -532 * counter + "px)"
 })
 
-prev.addEventListener("click", function (){
+prev[0].addEventListener("click", function (){
     if(counter === 0){
         return
     }
     counter--
-    console.log(counter);
     slider.style.transform = "translateX(" + -532 * counter + "px)"
 })
 
