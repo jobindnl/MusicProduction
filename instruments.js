aeb8cb764deb466bcdaf0c056f2efcee229a9029
jobindnl <jobindnl@gmail.com>
2021-03-03 00:43:12 -0500

    photo slides once per click

--- instruments.js
+++ instruments.js
@@ -1,10 +1,19 @@
 let photos = document.querySelectorAll("img")
 let prev = document.getElementsByClassName("prev")
 let next = document.getElementsByClassName("next")
-let slider = document.getElementsByClassName("slider")
-slider = slider[0]
+let main = document.querySelector("main")
 
 let counter = 0
+
+main.addEventListener('click', function(e){
+    if(e.target.className === 'next'){
+        console.log(e);
+        let slide = e.target.parentElement.previousElementSibling
+        slide.style.transform = 'translateX(-300px)'
+    } else if (e.target.className === 'prev'){
+    
+    }
+})
 
 // next[0].addEventListener("click", function (){
 //     if(counter === 24 ){
